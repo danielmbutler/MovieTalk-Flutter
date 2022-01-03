2a0fe5c75d66426ef0be190bc288b6ab653faf6c
Daniel Butler <danielmbutler@hotmail.co.uk>
2022-01-03 22:09:39 +0000

    updating server status to stateful widget

--- app/lib/screens/widgets/server_status.dart
+++ app/lib/screens/widgets/server_status.dart
@@ -1,28 +1,18 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class ServerStatus extends StatefulWidget {
-   final String message;
-   final bool isServerRunning;
-   ServerStatus(this.message,this.isServerRunning);
-
-  @override
-  _ServerStatusState createState() => _ServerStatusState(message, isServerRunning);
-}
-
-class _ServerStatusState extends State<ServerStatus> {
+class ServerStatus extends StatelessWidget {
   final String message;
   final bool isServerRunning;
-  _ServerStatusState(this.message, this.isServerRunning);
+
+  ServerStatus(this.message, this.isServerRunning);
 
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.end,
       children: [
-        Text(
-            "Movie Talk Server Status: $message"
-        ),
+        Text("Movie Talk Server Status: $message"),
         const Padding(padding: EdgeInsets.only(right: 6.0)),
         Container(
           width: 10.0,
@@ -30,7 +20,9 @@ class _ServerStatusState extends State<ServerStatus> {
           decoration: BoxDecoration(
             color: isServerRunning ? Colors.green : Colors.red,
             shape: BoxShape.circle,
-          ),)
+          ),
+        ),
+        const Padding(padding: EdgeInsets.only(right: 6.0)),
       ],
     );
   }
