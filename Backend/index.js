0452080c4b2c87e77047fba24f6ac597d4bdba14
Daniel Butler <danielmbutler@hotmail.co.uk>
2022-01-06 22:38:26 +0000

    created function to preseed datastore

--- Backend/index.js
+++ Backend/index.js
@@ -1,28 +1,18 @@
  // api and dynamodb setup
  var req = require('request');
- var AWS = require('aws-sdk');
- AWS.config.update({region: 'us-east-1'});
- var dynamodb = new AWS.DynamoDB();
+ require('isomorphic-fetch');
+
+
 
     // api params
-    const params = {
-        url: 'api key',
+    const movieApiParams = {
+        url: 'apikey',
         headers: { 'Content-Type': 'application/json' },
        
     };
+    
 
-    function insertToDB(item){
-        dynamodb.putItem({TableName: 'MovieTalk', Item: item}, function(err, data){
-            if (err) {
-            console.log(err); // an error occurred
-            } else {
-            // successful response
-            console.log("inserted");
-            }
-        });
-    }
-
-    req.post(params, function(err, res, body) {
+    req.post(movieApiParams, function(err, res, body) {
         if(err){
             console.log('------error------', err);
         } else{
@@ -39,13 +29,26 @@
             // add obj to movieList to insert into dynamodb
 
             for (var i = 0; i < movieList.length; i++) {
-                const movie = {
-                    "Title" : {"S" : movieList[i].original_title},
-                    "rating" : {"N" : String(movieList[i].vote_average)},
-                    "img": {"S" : movieList[i].poster_path},
-                    "overview": {"S" : movieList[i].overview}
-                };
-                insertToDB(movie);
+                var requestBody =  JSON.stringify({ query: `
+                mutation MyMutation {
+                    createMovieModel(input: {img: "` + movieList[i].poster_path + `", latestMessage: "", latestMessageTime: "", overview: "` +movieList[i].overview +`", rating: "`+ String(movieList[i].vote_average) + `", title: "` + movieList[i].original_title + `"}) {
+                    img
+                    latestMessage
+                    latestMessageTime
+                    overview
+                    rating
+                    title
+                    }
+                }` 
+                })
+                console.log(requestBody)
+                  fetch('', {
+                        method: 'POST',
+                        headers: { 'Content-Type': 'application/json', 'x-api-key' : '' },
+                        body: requestBody,
+                        })
+                        .then(res => res.json())
+                        .then(res => console.log(res));
             }; 
         }
     });
--- app/lib/screens/chat/widgets/message.dart
+++ app/lib/screens/chat/widgets/message.dart
@@ -14,13 +14,10 @@ class Message extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      // add some padding
       padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
       child: Align(
-        // align the child within the container
         alignment: isCurrentUser ? Alignment.centerRight : Alignment.centerLeft,
         child: DecoratedBox(
-          // chat bubble decoration
           decoration: BoxDecoration(
             color: isCurrentUser ? Colors.red : Colors.grey[300],
             borderRadius: BorderRadius.circular(16),
